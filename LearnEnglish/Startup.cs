4aa036d5961673bcd9cf733877115cf84290ce11
BSSoper <BSSoper@BSS-PC>
2016-11-07 10:02:10 +0200

    Add project files.

---
+++ LearnEnglish/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnEnglish.Startup))]
+namespace LearnEnglish
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
