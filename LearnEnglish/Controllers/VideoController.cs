420a144c901df836f2b368874d16a06b82aa29ca
BSSoper <BSSoper@BSS-PC>
2016-11-10 08:50:48 +0200

    test commit

---
+++ LearnEnglish/Controllers/VideoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace LearnEnglish.Controllers
+{
+    public class VideoLessonsController : Controller
+    {
+        // GET: Video
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
