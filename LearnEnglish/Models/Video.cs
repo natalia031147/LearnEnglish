8fe4f4b5396cef4c6b60bf2302182815c12ddfd1
Mursa Catalin Marian <catalin.mursa@expertnetwork.ro>
2016-12-11 16:36:34 +0200

    add mdf

---
+++ LearnEnglish/Models/Video.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearnEnglish.Models
+{
+    public class Video
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public string Thumbnail { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [Required]
+        public int Level { get; set; }
+    }
+
+    public class VideoMv
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Language { get; set; }
+        public string Thumbnail { get; set; }
+        public VideoPart[] parts { get; set; }
+    }
+
+    public class VideoPart
+    {
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public string native { get; set; }
+        public string source { get; set; }
+    }
+
+}
